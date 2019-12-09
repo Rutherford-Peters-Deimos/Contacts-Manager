d30ed25f43b18a20b2482eedfef1dcc1cf784a8d
Joshua Peters <peters.l.joshua@gmail.com>
2019-12-09 11:34:27 -0600

    saving project setup

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,2 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class MainMenu {
+
+    static final String directory = "contactslist";
+    static final String filename = "contacts";
+
+
+    public static void main(String[] args) {
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+
+        try {
+            if (Files.notExists(dataDirectory)) {
+                Files.createDirectories(dataDirectory);
+            }
+
+            if (!Files.exists(dataFile)) {
+                Files.createFile(dataFile);
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
 }
